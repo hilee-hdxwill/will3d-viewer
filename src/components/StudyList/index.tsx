5d37065fc6effe4228df8b20e567f7ceb83ccde9
biwoo-hdxwill <140477718+biwoo-hdxwill@users.noreply.github.com>
2024-11-22 11:52:53 +0900

    refactor: rename button name

--- src/components/StudyList/index.tsx
+++ src/components/StudyList/index.tsx
@@ -89,7 +89,7 @@ export function StudyList({ onBack, onViewImages }: StudyListProps) {
                   onClick={() => onViewImages(study.studyInstanceUID)}
                   className="mt-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
                 >
-                  View Images
+                  Rendering Viewer
                 </button>
               </div>
             </div>
